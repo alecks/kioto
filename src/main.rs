f32842154b6f942488ba1a6b01ce9716bfc4b784
Alex P <alexeek@protonmail.com>
2020-08-01 00:32:56 +0100

    ✨ Use Settings struct

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,21 @@ use env_logger::Env;
 use tokio;
 
 mod discord;
+mod settings;
+use settings::Settings;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
+
+    let cfg = Settings::new().expect("Couldn't load config");
 
     tokio::spawn(async { discord::init().await.unwrap() });
     HttpServer::new(|| {
         App::new().wrap(Logger::default())
         //.service(web::scope("/api/v1").configure(routes::version_one::config))
     })
-    .bind("0.0.0.0:8080")?
+    .bind(cfg.http.address)?
     .run()
     .await
 }
