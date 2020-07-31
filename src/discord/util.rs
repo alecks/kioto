5687e998cb101674aa97af80c0f4adcb0d021fcf
Alex P <alexeek@protonmail.com>
2020-08-01 00:32:40 +0100

    ✨ Implement TypeMapKey for Settings

--- src/discord/util.rs
+++ src/discord/util.rs
@@ -1,8 +1,18 @@
-use serenity::{prelude::{Mutex, TypeMapKey}, client::bridge::gateway::ShardManager};
+use crate::settings::Settings as FSettings;
+use serenity::{
+    client::bridge::gateway::ShardManager,
+    prelude::{Mutex, TypeMapKey},
+};
 use std::sync::Arc;
 
 pub struct ClientShardManager;
 
 impl TypeMapKey for ClientShardManager {
     type Value = Arc<Mutex<ShardManager>>;
+}
+
+pub struct Settings;
+
+impl TypeMapKey for Settings {
+    type Value = Arc<FSettings>;
 }
