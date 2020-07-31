ea21f43a0c6b4ac4485c9bde793ca3a3918e40d6
Alex P <alexeek@protonmail.com>
2020-07-31 23:07:14 +0100

    ✨ Add actix server and bot spawner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+use actix_web::{middleware::Logger, App, HttpServer};
+use env_logger::Env;
+use tokio;
+
+mod discord;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+
+    tokio::spawn(async { discord::init().await.unwrap() });
+    HttpServer::new(|| {
+        App::new().wrap(Logger::default())
+        //.service(web::scope("/api/v1").configure(routes::version_one::config))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
