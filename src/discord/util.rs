49fdf78c4b485c87e8b5b02cae872f39d6a70007
Alex P <alexeek@protonmail.com>
2020-08-01 23:32:27 +0100

    🐛 Implement TypeMapKey for DbPool

--- src/discord/util.rs
+++ src/discord/util.rs
@@ -1,3 +1,4 @@
+use crate::util::DbPool as PrevDbPool;
 use serenity::{
     client::bridge::gateway::ShardManager,
     prelude::{Mutex, TypeMapKey},
@@ -8,4 +9,10 @@ pub struct ClientShardManager;
 
 impl TypeMapKey for ClientShardManager {
     type Value = Arc<Mutex<ShardManager>>;
+}
+
+pub struct DbPool;
+
+impl TypeMapKey for DbPool {
+    type Value = Arc<PrevDbPool>;
 }
