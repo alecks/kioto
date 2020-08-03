fd73efb03cab22eec9474af962d7c064ec3ed63b
Alex P <alexeek@protonmail.com>
2020-08-03 08:51:27 +0100

    ✨ Put oauth client in app data

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ mod api;
 mod discord;
 mod routes;
 mod util;
-use util::Settings;
+use util::{AppState, Settings};
 
 use std::sync::Arc;
+
+use oauth2::basic::BasicClient;
+use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
+
+const AUTH_URL: &str = "https://discord.com/api/oauth2/authorize";
+const TOKEN_URL: &str = "https://discord.com/api/oauth2/token";
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -32,8 +38,28 @@ async fn main() -> std::io::Result<()> {
     }
 
     HttpServer::new(move || {
+        let auth_url = AuthUrl::new(AUTH_URL.to_string()).unwrap();
+        let token_url = TokenUrl::new(TOKEN_URL.to_string()).unwrap();
+
+        let cfg = Settings::get();
+        let discord_client_id = ClientId::new(cfg.bot.client_id);
+        let discord_client_secret = ClientSecret::new(cfg.bot.client_secret);
+
+        let oauth_client = BasicClient::new(
+            discord_client_id,
+            Some(discord_client_secret),
+            auth_url,
+            Some(token_url),
+        )
+        .set_redirect_url(
+            RedirectUrl::new(format!("{}/api/v1/auth/callback", cfg.http.url))
+                .expect("Invalid redirect URL"),
+        );
+
         App::new()
-            .data(pool.clone())
+            .data(AppState {
+                oauth: oauth_client,
+            })
             .wrap(Logger::default())
             .configure(routes::config)
     })
